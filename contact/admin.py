52319a2154549ed626ac852453f8c19b71780b90
Alberto Júnior <albertojunior206@gmail.com>
2025-09-25 20:21:51 -0300

    Model contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from contact import models
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = 'first_name',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 1000
+    list_display_links = 'id', 'first_name', 'phone'
