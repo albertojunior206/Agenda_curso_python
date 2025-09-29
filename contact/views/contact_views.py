fd874f246dc24d06937c09465f5525c8a0a8f807
Alberto Júnior <albertojunior206@gmail.com>
2025-09-29 07:28:24 -0300

    Paginação dos contatos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 from django.http import Http404
+from django.core.paginator import Paginator
 
 def index(request):
     contacts = Contact.objects.filter(show=True)
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -18,13 +25,46 @@ def index(request):
 def contact(request, contact_id):
     #single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(
         request,
         'contact/contact.html',
+        context
+    )
+
+def search(request):
+
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+    .filter(show=True)\
+    .filter(
+        Q(first_name__icontains = search_value) | 
+        Q(last_name__icontains = search_value) |
+        Q(phone__icontains = search_value) |
+        Q(email__icontains = search_value) 
+    )
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
         context
     )
