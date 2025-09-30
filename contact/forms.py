c05ee9d6394d1597a4bd723f3c7563478c20c60a
Alberto Júnior <albertojunior206@gmail.com>
2025-09-30 14:49:40 -0300

    pausa para estudar php

--- contact/forms.py
+++ contact/forms.py
@@ -1,16 +1,54 @@
 from django import forms
 from . import models
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
+
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
     class Meta:
         model = models.Contact
         fields = (
-            'first_name', 'last_name', 'phone', 'email', 'description', 'category'
+            'first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture'
         )
 
     def clean(self):
         cleaned_data = self.cleaned_data
 
         return super().clean()
+    
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required = True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required = True,
+        min_length=3,
+    )
+    email = forms.EmailField(
+        required = True,
+        min_length=3,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email', 'username', 'password1', 'password2',
+        )
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('E-mail já cadastrado', code='invalid')
+            )
+        return email
--- contact/urls.py
+++ contact/urls.py
@@ -6,8 +6,14 @@ app_name = 'contact'
 urlpatterns = [
     path('search/', views.search, name = 'search'),
     path('', views.index, name = 'index'),
+
+    #Contact (CRUD)
     path('contact/<int:contact_id>/detail', views.contact, name = 'contact'),
     path('contact/create', views.create, name = 'create'),
-    path('contact/<int:contact_id>/update', views.update, name = 'update'),
+    path('contact/<int:contact_id>/update/', views.update, name = 'update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name = 'delete'),
+
+    # User
+    path('user/create', views.register, name='register'),
 
 ]
